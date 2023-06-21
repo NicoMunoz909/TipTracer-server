4f1a3577b209e5082909fa243d46968a91ca688b
NicoMunoz909 <nimunoz28@gmail.com>
2023-06-21 08:38:06 -0500

    Create Header

---
+++ client/src/Components/Header/index.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import "./Header.css";
+import { GiHamburgerMenu } from "react-icons/gi";
+import logo from "../../Assets/logo.png";
+
+function Header() {
+  return (
+    <div className="header-container">
+      <div className="header__menu">
+        <GiHamburgerMenu />
+      </div>
+      <div className="header__icon">
+        <img src={logo} alt="" />
+      </div>
+      <div className="header__date">
+        <p>Lun 05/06</p>
+        <p>18:26</p>
+      </div>
+    </div>
+  );
+}
+
+export default Header;
