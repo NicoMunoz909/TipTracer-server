beb29da260856286f5f2a659937eb37d3647f20d
NicoMunoz909 <nimunoz28@gmail.com>
2023-06-29 20:15:03 -0500

    Make datebar for days

--- client/src/Components/Datebar/index.jsx
+++ client/src/Components/Datebar/index.jsx
@@ -2,51 +2,54 @@ import "./Datebar.css";
 import { TiArrowSync } from "react-icons/ti";
 
 const Datebar = () => {
-  const route = window.location.pathname;
+  const dateObejct = new Date(window.location.search.slice(-10) + "T00:00:00");
+  const dateString = window.location.search.slice(-10);
+  const daysArray = [
+    "Domingo",
+    "Lunes",
+    "Martes",
+    "Miercoles",
+    "Jueves",
+    "Viernes",
+    "Sabado",
+  ];
+  const monthArrays = [
+    "Enero",
+    "Febrero",
+    "Marzo",
+    "Abril",
+    "Mayo",
+    "Junio",
+    "Julio",
+    "Agosto",
+    "Septiembre",
+    "Octubre",
+    "Noviembre",
+    "Diciembre",
+  ];
 
   return (
     <div className="datebar-container">
-      {route === "/dia" && (
-        <div className="datebar__date">
-          <p>Lunes</p>
-          <div>
-            <p>05 Junio 2023</p>
+      <div className="datebar__date">
+        <p>{daysArray[dateObejct.getDay()]}</p>
+        <div>
+          <p>
+            {dateObejct.getDate() + " "}
+            {monthArrays[dateObejct.getMonth()] + " "}
+            {dateObejct.getFullYear()}
+          </p>
+          <form action="">
+            <input
+              type="date"
+              defaultValue={dateString}
+              id="fecha"
+              name="fecha"
+              onChange={(e) => e.target.form.submit()}
+            ></input>
             <TiArrowSync />
-          </div>
+          </form>
         </div>
-      )}
-      {route === "/semana" && (
-        <div className="datebar__date">
-          <p>Semana</p>
-          <div>
-            <p>Lun. 05/06 - Dom. 11/06</p>
-            <TiArrowSync />
-          </div>
-        </div>
-      )}
-      {route === "/mes" && (
-        <div className="datebar__date">
-          <p>Mes</p>
-          <div>
-            <p>Junio 2023</p>
-            <TiArrowSync />
-          </div>
-        </div>
-      )}
-      {route === "/custom" && (
-        <div className="datebar__date">
-          <div>
-            <p>Desde: </p>
-            <p>05/06/23</p>
-            <TiArrowSync />
-          </div>
-          <div>
-            <p>Hasta: </p>
-            <p>14/06/23</p>
-            <TiArrowSync />
-          </div>
-        </div>
-      )}
+      </div>
     </div>
   );
 };
