966dbbced5d267375b4280bc3e79fa931b6617ff
NicoMunoz909 <nimunoz28@gmail.com>
2023-07-15 21:52:55 -0500

    Add .env file

--- config/database.js
+++ config/database.js
@@ -1,3 +1,5 @@
+require("dotenv").config();
+
 module.exports = {
   development: {
     username: "root",
@@ -14,10 +16,10 @@ module.exports = {
     dialect: "mysql",
   },
   production: {
-    username: "root",
-    password: null,
-    database: "database_production",
-    host: "127.0.0.1",
+    username: process.env.DATABASE_USERNAME,
+    password: process.env.DATABASE_PASSWORD,
+    database: process.env.DATABASE_NAME,
+    host: process.env.DATABASE_HOSTNAME,
     dialect: "mysql",
   },
 };
