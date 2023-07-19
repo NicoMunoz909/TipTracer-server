d864942e28274a43f63cf408e9c526d3cce68438
NicoMunoz909 <nimunoz28@gmail.com>
2023-07-19 18:34:16 -0500

    Prepare for deploy

--- server.js
+++ server.js
@@ -2,13 +2,11 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const { sequelize } = require("./db/models");
-const path = require("path");
 const app = express();
 const port = process.env.PORT || 4000;
 
 app.use(cors());
 app.use(express.json());
-app.use(express.static(path.join(__dirname, "client", "dist")));
 
 //Routes
 const {
@@ -18,15 +16,11 @@ const {
   updateTable,
   deleteTable,
 } = require("./controllers/mesas");
-app.get("/api", getAll);
-app.get("/api/:id", getById);
-app.post("/api", createTable);
-app.patch("/api/:id", updateTable);
-app.delete("/api/:id", deleteTable);
-
-app.get("*", (req, res, next) => {
-  res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
-});
+app.get("/", getAll);
+app.get("/:id", getById);
+app.post("/", createTable);
+app.patch("/:id", updateTable);
+app.delete("/:id", deleteTable);
 
 app.listen(port, () => {
   console.log(`Server listening on port ${port}`);
@@ -37,9 +31,6 @@ const testDbConnection = (async () => {
     await sequelize.authenticate();
     console.log("Database connected succesfully.");
   } catch (error) {
-    console.error(
-      "Unable to connect to the database:",
-      error.original.sqlMessage
-    );
+    console.error("Unable to connect to the database:", error.original);
   }
 })();
