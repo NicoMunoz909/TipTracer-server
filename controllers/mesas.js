b8e23362ddad2670c88792cf91dd039f9ce260cf
NicoMunoz909 <nimunoz28@gmail.com>
2023-08-06 13:16:18 -0500

    Modify get request to use body. Add error catching

--- controllers/mesas.js
+++ controllers/mesas.js
@@ -2,48 +2,65 @@ const { Op } = require("sequelize");
 const { Mesas } = require("../db/models");
 
 const getAll = async (req, res) => {
-  const query = { ...req.query };
+  const body = { ...req.body };
   const filters = { where: {} };
-  for (const key in query) {
-    if (Object.hasOwnProperty.call(query, key)) {
-      if (key === "fechaDesde" || key === "fechaHasta") {
-        filters.where["fecha"] = {
-          [Op.between]: [query.fechaDesde, query.fechaHasta],
-        };
-      } else {
-        filters.where[key] = query[key];
+  try {
+    for (const key in body) {
+      if (Object.hasOwnProperty.call(body, key)) {
+        if (key === "fechaDesde" || key === "fechaHasta") {
+          filters.where["fecha"] = {
+            [Op.between]: [body.fechaDesde, body.fechaHasta],
+          };
+        } else {
+          filters.where[key] = body[key];
+        }
       }
     }
+    const mesas = await Mesas.findAll(filters);
+    res.send(mesas);
+  } catch (error) {
+    res.status(500).send(error.original.sqlMessage);
   }
-  const mesas = await Mesas.findAll(filters);
-  res.send(mesas);
 };
 
 const getById = async (req, res) => {
-  const mesa = await Mesas.findByPk(req.params.id);
-  res.send(mesa);
+  try {
+    const mesa = await Mesas.findByPk(req.params.id);
+    res.send(mesa);
+  } catch (error) {
+    res.status(500).send(error.original.sqlMessage);
+  }
 };
 
 const createTable = async (req, res) => {
-  const nuevaMesa = await Mesas.create({ ...req.body });
-  res.status(200).send(nuevaMesa);
+  try {
+    const nuevaMesa = await Mesas.create({ ...req.body });
+    res.status(200).send("Mesa creada exitosamente");
+  } catch (error) {
+    res.status(500).send(error.original.sqlMessage);
+  }
 };
 
 const updateTable = async (req, res) => {
-  const mesa = await Mesas.update(
-    { ...req.body },
-    { where: { id: req.params.id } }
-  );
-  res.status(200).send(mesa);
+  try {
+    const mesa = await Mesas.update({ ...req.body }, { where: { id: req.params.id } });
+    res.status(200).send("Mesa modificada exitosamente");
+  } catch (error) {
+    res.status(500).send(error.original.sqlMessage);
+  }
 };
 
 const deleteTable = async (req, res) => {
-  const mesaBorrada = await Mesas.destroy({
-    where: {
-      id: req.params.id,
-    },
-  });
-  res.sendStatus(200).send(mesaBorrada);
+  try {
+    const mesaBorrada = await Mesas.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+    res.sendStatus(200).send("Mesa borrada exitosamente");
+  } catch (error) {
+    res.status(500).send(error.original.sqlMessage);
+  }
 };
 
 module.exports = { getAll, getById, createTable, updateTable, deleteTable };
--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const { sequelize } = require("./db/models");
 const app = express();
 const port = process.env.PORT || 4000;
 
-app.use(cors({ origin: "*" }));
+app.use(cors());
 app.use(express.json());
 
 //Routes
